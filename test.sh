908d2ac0fcfb42da1ee946f4a92bba0f42e2950d
JoshuaSkootsky <joshua.skootsky@gmail.com>
2016-05-31 10:45:27 -0400

    combined recompile into test

--- test.sh
+++ test.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-./random_tester < random_tester.inp
+# Recompiles program, runs it with sample input
+
+
+file="random_tester"
+# You need the space around the brackets
+if [ -f "$file" ]
+then
+    rm "$file" && make "$file"
+else
+    make "$file"
+fi
+
+"./$file" < "$file.inp"

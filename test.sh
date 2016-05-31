ebe9ec3513b01668016ac8e64e2d5b1ff1d7b287
JoshuaSkootsky <joshua.skootsky@gmail.com>
2016-05-31 15:28:00 -0400

    test.sh separated from recompile

--- test.sh
+++ test.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-# Recompiles program, runs it with sample input
-
+# runs with sample input
 
 file="random_tester"
-# You need the space around the brackets
-if [ -f "$file" ]
-then
-    rm "$file" && make "$file"
-else
-    make "$file"
-fi
-
 "./$file" < "$file.inp"
